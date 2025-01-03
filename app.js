d796190b27f8de570ce5c3c555d136f471655bf8
achrefmechergui <achreffcb123achref@gamil.com>
2025-01-03 21:55:12 +0100

    js-day-01

---
+++ app.js
@@ -0,0 +1,81 @@
+
+///// variables ::: 
+
+// let firstName = "jonas";   ///// data types : string , number , boolean
+// console.log( typeof firstName)
+
+
+// console.log(firstName) ;
+
+// firstName = "mohamed"
+
+// console.log(firstName)
+
+// var y = "ay haja"
+// console.log(y)
+// y = "500"
+// console.log(y)    //// mutate  ====>("tnajim tbadel el valeurs")
+///// constant :::: 
+/// ===> imutable variables ... (sta3melet el const )
+
+// const x = "hello" ; 
+// console.log(x)
+
+// x = "good by"
+
+// console.log(x)
+
+//   const now = 2025 ; 
+
+  ///// dry == > d'ont repeat yourself .../////////
+//   const maxAge = now - 1990 ; 
+//   const aliAge = now - 2000 ; 
+
+
+//   console.log(maxAge)
+//   console.log(aliAge)
+
+
+// let x = 5 + 3 ; 
+// console.log(x)  //// 8
+
+// // x = x + 10 ;   /// x = 8 +10 ; 
+// // console.log(x)
+
+
+// x+=10 ;   //  x = x +10 ; 
+// console.log(x) ; 
+
+
+// let y = 10 ; 
+
+// y = y - 5 ; 
+// y-=5 ; 
+// console.log(y) ; 
+
+// let z = 10 ; 
+
+// z = z*2 ; 
+// z*=2
+// console.log(z)
+
+
+//// increment 
+// let a = 1 ;
+// // a = a +1 
+// a++
+
+
+// console.log(a)
+/// decrement :: 
+// let b = 10 ; 
+// b = b -1 
+// b-- ;
+// console.log(b)
+
+// let maxAge = 35 ; 
+// let aliAge = 25 ;
+// let age = maxAge < aliAge
+// console.log(typeof age) ; 
+
+
